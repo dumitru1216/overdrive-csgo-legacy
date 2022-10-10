f97be16bf8aa44b43f86107fdec0f25cfadde088
dumitru1216 <46423190+dumitru1216@users.noreply.github.com>
2022-10-10 14:02:40 -0700

    fixed is_hltv

--- overdrive_csgo/hack/hooks/vmt/v_is_hltv.cpp
+++ overdrive_csgo/hack/hooks/vmt/v_is_hltv.cpp
@@ -5,7 +5,7 @@
 #include "../../globals.h"
 
 auto ptr_accumulate_layers = reinterpret_cast< void* > ( utils::pattern_scan( "client.dll", "84 C0 75 0D F6 87" ) );
-auto setupvelocity_call = reinterpret_cast< void* > ( utils::pattern_scan( ( "client.dll" ), ( "84 C0 75 38 8B 0D ? ? ? ? 8B 01 8B 80 ? ? ? ? FF D0" ) ) );
+auto setupvelocity_call = reinterpret_cast< void* > ( utils::pattern_scan( ( "client.dll" ), ( "84 C0 75 38 8B 0D ? ? ? ? 8B 01 8B 80" ) ) );
 
 using is_hltv_t = bool( __fastcall* ) ( );
 
@@ -16,8 +16,8 @@ bool __fastcall hooks::is_hltv( )
 	if ( m_inUnhook )
 		return org_f( );
 
-	if ( reinterpret_cast< uintptr_t > ( _ReturnAddress( ) ) == reinterpret_cast< uintptr_t > ( ptr_accumulate_layers ) )
-		return true;
+	//if ( reinterpret_cast< uintptr_t > ( _ReturnAddress( ) ) == reinterpret_cast< uintptr_t > ( ptr_accumulate_layers ) )
+		//return true;
 
 	if ( reinterpret_cast< uintptr_t > ( _ReturnAddress( ) ) == reinterpret_cast< uintptr_t > ( setupvelocity_call ) && csgo::m_allow_call )
 		return true;
